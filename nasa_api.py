6c81904327ea4e3d8a05e44551fbfc38f775b226
Marco Marquez <marcomarquez.a1@gmail.com>
2026-06-22 17:26:27 +0000

    Added NASA API data loading into SQLite database

--- nasa_api.py
+++ nasa_api.py
@@ -1,12 +1,41 @@
 import requests
+import pandas as pd
+import sqlalchemy as db
 
+# Get NASA API data
 API_KEY = "M3CCsNeHMKXdjJjzrkhwfx0SEsAEOR5MCe686jvp"
+
 url = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
 
 response = requests.get(url)
 
+
 print(response.status_code)
-print(response.json())
+print(response.text)
+
+data = response.json()
+
+print(response.status_code)
 
 
+# Convert dictionary into pandas dataframe
+df = pd.DataFrame.from_dict([data])
 
+print(df)
+
+
+# Import SQLAlchemy and create database engine
+engine = db.create_engine('sqlite:///nasa.db')
+
+
+# Send dataframe to SQL table
+df.to_sql('apod', con=engine, if_exists='replace', index=False)
+
+
+# Query database and print results
+with engine.connect() as connection:
+    query_result = connection.execute(
+        db.text("SELECT * FROM apod;")
+    ).fetchall()
+
+    print(pd.DataFrame(query_result))
